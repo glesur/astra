3b8093596244726dea9e5da9a7a9acd8d06f94c4
Geoffroy Lesur <geoffroy.lesur@univ-grenoble-alpes.fr>
2025-09-22 16:44:01 +0200

    missing mpi file

---
+++ src/mpi.hpp
@@ -0,0 +1,20 @@
+// ***********************************************************************************
+// ASTRA spectral code
+// Accelerated Spectral code for TuRbulent plasmA
+// Copyright(C) Geoffroy R. J. Lesur <geoffroy.lesur@univ-grenoble-alpes.fr>
+// and other code contributors
+// Licensed under CeCILL 2.1 License, see COPYING for more information
+// ***********************************************************************************
+
+#ifdef WITH_MPI
+
+#ifndef MPI_HPP_
+#define MPI_HPP_
+#include <mpi.h>
+
+//redefined MPI complex type to match Astra Complex type
+#define MPI_Astra_Complex  MPI_C_DOUBLE_COMPLEX
+
+
+#endif // MPI_HPP_
+#endif // WITH_MPI
